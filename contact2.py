c88e6decef82c183f691ed3e3e010c780a4664cf
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-16 14:15:34 +0700

    class contact

---
+++ contact2.py
@@ -0,0 +1,11 @@
+
+
+class Contact:
+
+    def __index__(self, firstname, lastname, address, home, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.home = home
+        self.email = email
+
