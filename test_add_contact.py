58152de6feeeda71a4c1ae15fcfb86689eae2de3
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-16 09:58:36 +0700

    Выделены вспомогательные методы

--- test_add_contact.py
+++ test_add_contact.py
@@ -16,9 +16,18 @@ class test_add_contact(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_test_add_contact(self):
-        success = True
         wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_add_new_page(wd)
+        self.fill_contact_form(wd)
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -29,7 +38,11 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_add_new_page(self, wd):
         wd.find_element_by_link_text("add new").click()
+
+    def fill_contact_form(self, wd):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("julia")
@@ -54,11 +67,17 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
         wd.find_element_by_name("email2").send_keys("altay.2015@mail.ru")
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.init_group_creation(wd)
+
+    def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    def init_group_creation(self, wd):
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
     def tearDown(self):
         self.wd.quit()
 
