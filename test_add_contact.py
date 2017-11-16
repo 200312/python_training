ff24136412e663910e03eee567d331a88e80298b
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-16 11:52:48 +0700

    Записан рекордеpом сценарий создания нового контакта

--- test_add_contact.py
+++ test_add_contact.py
@@ -12,22 +12,13 @@ def is_alert_present(wd):
 
 class test_add_contact(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
+        self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
     def test_test_add_contact(self):
+        success = True
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.open_add_new_page(wd)
-        self.fill_contact_form(wd)
-        self.return_to_home_page(wd)
-        self.logout(wd)
-
-    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
-
-    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -38,11 +29,7 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_add_new_page(self, wd):
         wd.find_element_by_link_text("add new").click()
-
-    def fill_contact_form(self, wd):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("julia")
@@ -67,17 +54,11 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
         wd.find_element_by_name("email2").send_keys("altay.2015@mail.ru")
-        self.init_group_creation(wd)
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def init_group_creation(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
+        wd.find_element_by_link_text("home page").click()
+        wd.find_element_by_link_text("Logout").click()
+        self.assertTrue(success)
+    
     def tearDown(self):
         self.wd.quit()
 
