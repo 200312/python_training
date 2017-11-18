6b8f133f85332b511cd0466f319e5ddb7bf2fd43
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-18 22:19:39 +0700

    class helper for contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from contact2 import Contact
 from fixture.application2 import Application2
+from model.contact2 import Contact
 
 
 @pytest.fixture
@@ -12,11 +12,11 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="fghfgthg", lastname="fghgjhgj", address="jgfjgjg", home="gjgjghjhk", email="hgjhjhjkkh"))
-    app.logout()
+    app.session2.login(username="admin", password="secret")
+    app.group2.create_contact(Contact(firstname="fghfgthg", lastname="fghgjhgj", address="jgfjgjg", home="gjgjghjhk", email="hgjhjhjkkh"))
+    app.session2.logout()
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="", lastname="", address="", home="", email=""))
-    app.logout()
+    app.session2.login(username="admin", password="secret")
+    app.group2.create_contact(Contact(firstname="", lastname="", address="", home="", email=""))
+    app.session2.logout()
