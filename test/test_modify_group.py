d3870bd5621f4114373b16934069b20c4eb42b87
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-27 16:51:38 +0700

    modify_group_test

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,10 +1,12 @@
-
 from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.group.modify_first_group(Group(name="New group"))
+    if app.group.count1 == 0:
+        app.group.create(Group(name="test1"))
+        app.group.modify_first_group(Group(name="New group"))
 
 def test_modify_group_header(app):
-    app.group.modify_first_group(Group(header="New header"))
+        app.group.create(Group(name="test1"))
+        app.group.modify_first_group(Group(header="New header"))
 
