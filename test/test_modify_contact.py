f5f16941437f54844c7a7b3f8764dc3cdf229dee
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-27 16:49:41 +0700

    прелусловия в тесте для модификации контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,9 +1,11 @@
-
 from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    app.contact.modify_first_contact(Contact(firstname="New contact"))
+    if app.contact.count2 == 0:
+        app.contact.create(Contact(firstname="test"))
+        app.contact.modify_first_contact(Contact(firstname="New contact"))
 
 def test_modify_contact_lastname(app):
-    app.contact.modify_first_contact(Contact(lastname="New lastname"))
+        app.contact.create(Contact(firstname="test"))
+        app.contact.modify_first_contact(Contact(lastname="New lastname"))
