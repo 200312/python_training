d93b4e568a9ba66cd1291526ee38b84ba3cbd519
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-28 07:52:00 +0700

    test_modify_group_count

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    if app.group.count1 == 0:
+    if app.group.count == 0:
         app.group.create(Group(name="test1"))
         app.group.modify_first_group(Group(name="New group"))
 
