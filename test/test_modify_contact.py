c073d1ad24699ea680aaaae3d8c0f9fb83c8e98d
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-28 18:16:43 +0700

    test_modify_contact_count

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    if app.contact.count2 == 0:
+    if app.contact.count == 0:
         app.contact.create(Contact(firstname="test"))
         app.contact.modify_first_contact(Contact(firstname="New contact"))
 
