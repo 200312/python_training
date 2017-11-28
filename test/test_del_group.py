76063fb7ab36e903ee3d59ffe15cd93c4e829698
Julia Gurianova <juliia.gurianova@gmail.com>
2017-11-28 18:17:55 +0700

    test_del_group_count

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,6 +1,6 @@
 from model.group import Group
 
 def test_delete_first_group(app):
-    if app.group.count2() == 0:
-        app.contact.create_contact(Group(name="test"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.delete_first_group()
