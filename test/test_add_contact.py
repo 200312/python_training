6577291d7b006379b43debda5f93f52700f2de54
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-03 19:29:22 +0700

    сравнение контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
+    old_contacts = app.contact.get_contact_list()
     app.contact.create_contact(Contact(firstname="fghfgthg", lastname="fghgjhgj", address="jgfjgjg", home="gjgjghjhk", email="hgjhjhjkkh"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
 
-def test_add_empty_contact(app):
-    app.contact.create_contact(Contact(firstname="", lastname="", address="", home="", email=""))
+#def test_add_empty_contact(app):
+   # app.contact.create_contact(Contact(firstname="", lastname="", address="", home="", email=""))
