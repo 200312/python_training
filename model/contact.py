b7e9e3a9b2586112ba2ebf45aa474324a4e3ec9f
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-05 13:31:04 +0700

    model_contact_id

--- model/contact.py
+++ model/contact.py
@@ -2,10 +2,11 @@
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, address=None, home=None, email=None):
+    def __init__(self, firstname=None, lastname=None, address=None, home=None, email=None, id=None):
         self.firstname = firstname
         self.lastname = lastname
         self.address = address
         self.home = home
         self.email = email
+        self.id = id
 
