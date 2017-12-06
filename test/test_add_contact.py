f8ed825d5c3bf9d5d03b8db009e5a3199080fce4
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-06 15:24:36 +0700

    test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+from sys import maxsize
 
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create_contact(Contact(firstname="fghfgthg", lastname="fghgjhgj", address="jgfjgjg", home="gjgjghjhk", email="hgjhjhjkkh"))
+    contact = Contact(firstname="fghfgthg", lastname="fghgjhgj", address="jgfjgjg", home="gjgjghjhk", email="hgjhjhjkkh")
+    app.contact.create_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    def id_or_max(cnt):
+        if cnt.id:
+            return int(cnt.id)
+        else:
+            return maxsize
+    assert sorted(old_contacts, key=id_or_max) == sorted(new_contacts, key=id_or_max)
 
 
 #def test_add_empty_contact(app):
-   # app.contact.create_contact(Contact(firstname="", lastname="", address="", home="", email=""))
+    #old_contacts = app.contact.get_contact_list()
+    #app.contact.create_contact(Contact(firstname="", lastname="", address="", home="", email=""))
+    #new_contacts = app.contact.get_contact_list()
+    #assert len(old_contacts) + 1 == len(new_contacts)
+
