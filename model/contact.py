81ccb7e7728c4c868534a75bc75c9333acef3ac4
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-06 15:23:01 +0700

    model_contact

--- model/contact.py
+++ model/contact.py
@@ -10,3 +10,10 @@ class Contact:
         self.email = email
         self.id = id
 
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.firstname)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
+
+
