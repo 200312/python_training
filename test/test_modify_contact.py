9e0ccb53733a48002f4d94240f41966ac7fe4ae6
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-06 09:26:48 +0700

    сравнение списков контактов_modify

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,10 +2,15 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    if app.contact.count == 0:
-        app.contact.create(Contact(firstname="test"))
+        old_contacts = app.contact.get_contact_list()
         app.contact.modify_first_contact(Contact(firstname="New contact"))
+        new_contacts = app.contact.get_contact_list()
+        assert len(old_contacts) == len(new_contacts)
+
 
 def test_modify_contact_lastname(app):
-        app.contact.create(Contact(firstname="test"))
+        old_contacts = app.contact.get_contact_list()
         app.contact.modify_first_contact(Contact(lastname="New lastname"))
+        new_contacts = app.contact.get_contact_list()
+        assert len(old_contacts) == len(new_contacts)
+
