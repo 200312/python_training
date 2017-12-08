8a7bc9bb9c03d99f2242cab57ef3cc1dce6bb203
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-08 22:57:56 +0700

    add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,17 +10,17 @@ def test_add_contact(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-    def id_or_max(cnt):
-        if cnt.id:
-            return int(cnt.id)
-        else:
-            return maxsize
-    assert sorted(old_contacts, key=id_or_max) == sorted(new_contacts,key=id_or_max)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts,key=Contact.id_or_max)
 
 
-#def test_add_empty_contact(app):
-    #old_contacts = app.contact.get_contact_list()
-    #app.contact.create_contact(Contact(firstname="", lastname="", address="", home="", email=""))
-    #new_contacts = app.contact.get_contact_list()
-    #assert len(old_contacts) + 1 == len(new_contacts)
+def test_add_empty_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="", lastname="", address="", home="", email="")
+    app.contact.create_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
 
