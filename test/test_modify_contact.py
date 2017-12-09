339c129e8c944d4241a9b5905dfdf76ba3987528
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-09 07:20:54 +0700

    modify_contact

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,16 +1,19 @@
 from model.contact import Contact
 
-
 def test_modify_contact_firstname(app):
         old_contacts = app.contact.get_contact_list()
-        app.contact.modify_first_contact(Contact(firstname="New contact"))
+        contact = Contact(firstname="New contact")
+        contact.id = old_contacts[0].id
+        app.contact.modify_first_contact(contact)
         new_contacts = app.contact.get_contact_list()
         assert len(old_contacts) == len(new_contacts)
+        old_contacts[0] = contact
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_modify_contact_lastname(app):
-        old_contacts = app.contact.get_contact_list()
-        app.contact.modify_first_contact(Contact(lastname="New lastname"))
-        new_contacts = app.contact.get_contact_list()
-        assert len(old_contacts) == len(new_contacts)
+#def test_modify_contact_lastname(app):
+        #old_contacts = app.contact.get_contact_list()
+        #app.contact.modify_first_contact(Contact(lastname="New lastname"))
+        #new_contacts = app.contact.get_contact_list()
+        #assert len(old_contacts) == len(new_contacts)
 
