0e59b77292ba27ba2226c86a52d41636b1639475
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-10 09:28:02 +0700

    test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from sys import maxsize
+
 
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
     contact = Contact(firstname="fghfgthg", lastname="fghgjhgj", address="jgfjgjg", home="gjgjghjhk", email="hgjhjhjkkh")
     app.contact.create_contact(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts,key=Contact.id_or_max)
 
 
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="", lastname="", address="", home="", email="")
-    app.contact.create_contact(contact)
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+#def test_add_empty_contact(app):
+    #old_contacts = app.contact.get_contact_list()
+   # contact = Contact(firstname="", lastname="", address="", home="", email="")
+    #app.contact.create_contact(contact)
+   # new_contacts = app.contact.get_contact_list()
+   # assert len(old_contacts) + 1 == len(new_contacts)
+    #old_contacts.append(contact)
+   # assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
