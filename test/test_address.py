76ee1e91a3a7413d6d69c7184090c90c8aada5a1
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-14 10:21:06 +0700

    test_address

--- test/test_address.py
+++ test/test_address.py
@@ -6,11 +6,14 @@ def test_address_on_home_page(app):
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.secondaryaddress == contact_from_edit_page.secondaryaddress
 
-def test_address_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.address == contact_from_edit_page.address
-    assert contact_from_view_page.secondaryaddress == contact_from_edit_page.secondaryaddress
+#def test_address_on_contact_view_page(app):
+    #contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    #contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    #assert contact_from_view_page.address == contact_from_edit_page.address
+    #assert contact_from_view_page.secondaryaddress == contact_from_edit_page.secondaryaddress
+
+#def merge_address_like_on_home_page(contact):
+    #return "\n".join([contact.address, contact.secondaryaddress])
 
 
 
