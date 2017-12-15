3f0bfa3cdafb778095f9a111a3206e9ee2bbe90a
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-15 19:55:09 +0700

    test_lastname

--- test/test_lastname.py
+++ test/test_lastname.py
@@ -8,11 +8,11 @@ def test_lastname_on_home_page(app):
 def test_lastname_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_view_page.lastname == clear(contact_from_edit_page.lastname)
 
 def clear(s):
-    return re.sub("[\n]", "", s)
+    return re.sub("[ ]", "", s)
 
 def merge_lastname_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "", (map(lambda x: clear(x), [contact.lastname]))))
+    return "\n".join(map(lambda x: clear(x), [contact.lastname]))
 
