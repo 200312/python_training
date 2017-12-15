1f33fe859ba4cbcd045ea6b66724a142e76454ca
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-15 09:52:58 +0700

    test_address

--- test/test_address.py
+++ test/test_address.py
@@ -1,19 +1,19 @@
-
+import re
 
 def test_address_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.secondaryaddress == contact_from_edit_page.secondaryaddress
+    assert contact_from_home_page.address == clear(contact_from_edit_page.address)
+    assert contact_from_home_page.secondaryaddress == clear(contact_from_edit_page.secondaryaddress)
 
-#def test_address_on_contact_view_page(app):
-    #contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    #contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    #assert contact_from_view_page.address == contact_from_edit_page.address
-    #assert contact_from_view_page.secondaryaddress == contact_from_edit_page.secondaryaddress
+def test_address_on_contact_view_page(app):
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.address == clear(contact_from_edit_page.address)
+    assert contact_from_view_page.secondaryaddress == clear(contact_from_edit_page.secondaryaddress)
 
-#def merge_address_like_on_home_page(contact):
-    #return "\n".join([contact.address, contact.secondaryaddress])
+def clear(s):
+    return re.sub("[\n]", "", s)
 
 
 
