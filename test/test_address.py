6de7c88f30d85560f28ba7df67f3316ba2019e2c
Julia Gurianova <juliia.gurianova@gmail.com>
2017-12-16 09:56:28 +0700

    test_address

--- test/test_address.py
+++ test/test_address.py
@@ -3,8 +3,7 @@ import re
 def test_address_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.address == clear(contact_from_edit_page.address)
-    assert contact_from_home_page.secondaryaddress == clear(contact_from_edit_page.secondaryaddress)
+    assert contact_from_home_page.address_from_home_page == merge_address_like_on_home_page(contact_from_edit_page)
 
 def test_address_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
@@ -14,6 +13,9 @@ def test_address_on_contact_view_page(app):
 
 def clear(s):
     return re.sub("[\n]", "", s)
+
+def merge_address_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "", (map(lambda x: clear(x), [contact.address]))))
 
 
 
