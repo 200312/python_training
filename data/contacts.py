6ed20760c624e96e6a051857345382d29b9c03bf
200312 <juliia.gurianova@gmail.com>
2017-12-24 10:35:57 +0700

    contacts.py

--- data/contacts.py
+++ data/contacts.py
@@ -3,8 +3,8 @@ from model.contact import Contact
 
 testdata = [
     Contact(firstname="firstname1", lastname="lastname1", homephone="homephone1", mobilephone="mobilephone1", workphone="workphone1",
-                 secondaryphone="secondaryphone1", address="address1", secondaryaddress="secondaryaddress1", email="email1"),
-    Contact(firstname="firstname2", lastname="lastname2", homephone="homephone2", mobilephone="mobilephone2",
-            workphone="workphone2", secondaryphone="secondaryphone2", address="address2", secondaryaddress="secondaryaddress2", email="email2")
+            address="address1", email="email1"),
+    Contact(firstname="firstname2", lastname="lastname2", homephone="homephone2", mobilephone="mobilephone2", workphone="workphone2",
+            address="address2", email="email2")
 ]
 
