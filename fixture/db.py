a343996b074af5fea0096da5ee0342c1c6725ef7
200312 <juliia.gurianova@gmail.com>
2017-12-26 18:20:23 +0700

    db.py

--- fixture/db.py
+++ fixture/db.py
@@ -29,11 +29,11 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, home, mobile, work, address, email from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, home, mobile, work, address, email, email2, email3 from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname, home, mobile, work, address, email) = row
+                (id, firstname, lastname, home, mobile, work, address, email, email2, email3) = row
                 list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, homephone=home, mobilephone=mobile,
-                                    workphone=work, address=address, email=email))
+                                    workphone=work, address=address, email=email, email2=email2,  email3=email3))
         finally:
             cursor.close()
         return list
