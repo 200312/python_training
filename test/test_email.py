20aafbccba5b65d4d6eee78c54fc906f729c863b
200312 <juliia.gurianova@gmail.com>
2017-12-26 18:19:46 +0700

    test_email

--- test/test_email.py
+++ test/test_email.py
@@ -1,9 +1,12 @@
 import re
+from model.contact import Contact
 
-def test_email_on_home_page(app):
+def test_email_on_home_page(app, check_ui):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
+    if check_ui:
+        assert sorted(contact_from_home_page.all_email_from_home_page, key=Contact.id_or_max) == sorted(merge_email_like_on_home_page(contact_from_edit_page), key=Contact.id_or_max)
 
 
 def test_email_on_contact_view_page(app):
@@ -12,6 +15,8 @@ def test_email_on_contact_view_page(app):
     assert contact_from_view_page.email == contact_from_edit_page.email
     assert contact_from_view_page.email2 == contact_from_edit_page.email2
     assert contact_from_view_page.email3 == contact_from_edit_page.email3
+
+
 
 def merge_email_like_on_home_page(contact):
     return "\n".join([contact.email, contact.email2, contact.email3])
