10683691a15d3d8a4ce7d53b002b613eb33f327a
200312 <juliia.gurianova@gmail.com>
2017-12-26 15:47:32 +0700

    test_modify_group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,17 +1,19 @@
+
 from model.group import Group
 import random
 
 
-def test_modify_group_name(app, db):
+def test_modify_group_name(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
-    group = random.choice(old_groups), Group(name="New group")
+    group = random.choice(old_groups)
     app.group.modify_group_by_id(group.id)
     new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
     old_groups.remove(group)
-    assert sorted(old_groups,key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
