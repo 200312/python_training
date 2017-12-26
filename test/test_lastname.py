42ed7da72876d931ef2605d616f7301176abf6de
200312 <juliia.gurianova@gmail.com>
2017-12-26 18:02:40 +0700

    test_lastname

--- test/test_lastname.py
+++ test/test_lastname.py
@@ -1,9 +1,14 @@
 import re
+from model.contact import Contact
 
-def test_lastname_on_home_page(app):
+
+def test_lastname_on_home_page(app, check_ui):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.lastname_from_home_page == merge_lastname_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.lastname == merge_lastname_like_on_home_page(contact_from_edit_page)
+    if check_ui:
+        assert sorted(contact_from_home_page.lastname, key=Contact.id_or_max) == sorted(merge_lastname_like_on_home_page(contact_from_edit_page), key=Contact.id_or_max)
+
 
 def test_lastname_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
