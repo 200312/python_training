687a3b9ab22f1265605dc3e8dba05df9ae39d83c
200312 <juliia.gurianova@gmail.com>
2017-12-26 17:40:05 +0700

    test_address

--- test/test_address.py
+++ test/test_address.py
@@ -1,9 +1,13 @@
 import re
+from model.contact import Contact
 
-def test_address_on_home_page(app):
+def test_address_on_home_page(app, check_ui):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.address_from_home_page == merge_address_like_on_home_page(contact_from_edit_page)
+    if check_ui:
+        assert sorted(contact_from_home_page.address_from_home_page, key=Contact.id_or_max) == sorted(merge_address_like_on_home_page(contact_from_edit_page), key=Contact.id_or_max)
+
 
 def test_address_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
