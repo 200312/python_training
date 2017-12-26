0d2e6a2fc71caf41b501a157393dd210a1f2f414
200312 <juliia.gurianova@gmail.com>
2017-12-26 13:08:18 +0700

    fixture_group

--- fixture/group.py
+++ fixture/group.py
@@ -85,6 +85,7 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+
     def modify_group_by_id(self, id, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
