f09567401ff914f7da05adf584fbc3830bef5377
200312 <juliia.gurianova@gmail.com>
2017-12-30 12:13:34 +0700

    test_address

--- test/test_address.py
+++ test/test_address.py
@@ -2,7 +2,7 @@ import re
 from model.contact import Contact
 
 def test_address_on_home_page(app, check_ui):
-    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_home_page = app.contact.get_contact_list()
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.address_from_home_page == merge_address_like_on_home_page(contact_from_edit_page)
     if check_ui:
