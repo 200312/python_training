f14b92b11529f10b2a67a68f4e8bc3170bdd790d
200312 <juliia.gurianova@gmail.com>
2017-12-31 11:12:22 +0700

    test_add_contact_to_group

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -2,9 +2,8 @@
 from model.contact import Contact
 
 
-def test_add_contact_to_group(app, db, contact):
+def test_add_contact_to_group(app, db):
     old_contacts = db.get_contact_list()
-    app.contact.add_contact_to_group(contact)
+    app.contact.add_contact_to_group()
     new_contacts = db.get_contact_list()
-    old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
